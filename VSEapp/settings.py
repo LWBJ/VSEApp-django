25eef4e77e64385dacabb53778d830f84648e48b
Joel <lwbjoel@gmail.com>
2022-02-19 00:54:10 +0800

    commit

--- VSEapp/settings.py
+++ VSEapp/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-tkbjpv-)$q3*#+b6in-f$lzvwg+e!r)%d$%#za#aes9&!m%qm7'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'secretspoopykey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
